8c5240ed46aefba47cd280c3e32a592ea2fad243
GasanaJr <gasanadj2003@gmail.com>
2024-03-06 17:47:36 +0200

    bug(user): fix role

--- helpers/validation.js
+++ helpers/validation.js
@@ -8,6 +8,7 @@ const registerValidation = (data) => {
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
+    role: Joi.string(),
   });
   return schema.validate(data);
 };
